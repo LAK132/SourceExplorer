4cccc7f494b4ce00185cbae5a1ade24e4a8f6177
LAK132 <lakridesagain@gmail.com>
2019-03-13 23:32:58 +1030

    Add AwaitPopup

--- src/imgui_impl_lak.h
+++ src/imgui_impl_lak.h
@@ -251,6 +251,33 @@ namespace lak
         return finished;
     }
 
+    template<typename R, typename ...T, typename ...D>
+    bool AwaitPopup(
+        const char *str_id, bool &open,
+        std::thread *&staticThread, std::atomic<bool> &staticFinished,
+        R(*callback)(T...), const std::tuple<D...> &callbackData
+    )
+    {
+        if (ImGui::BeginPopup(str_id, ImGuiWindowFlags_AlwaysAutoResize))
+        {
+            if (lak::Await(staticThread, &staticFinished, callback, callbackData))
+            {
+                ImGui::CloseCurrentPopup();
+                ImGui::EndPopup();
+                open = false;
+                return false;
+            }
+            open = true;
+        }
+        else
+        {
+            open = false;
+            ImGui::OpenPopup(str_id);
+        }
+
+        return true;
+    }
+
     bool VertSplitter(
         float &left,
         float &right,
