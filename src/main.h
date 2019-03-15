a2d80f4982dd5953225079c55c0d4b7f67b97999
LAK132 <LAK132@kicodora.com>
2019-03-15 15:18:30 +1030

    Add Anaconda license to credits

--- src/main.h
+++ src/main.h
@@ -113,9 +113,24 @@ SOFTWARE.)");
     }
     if (ImGui::TreeNode("Anaconda/Chowdren"))
     {
-        ImGui::Text("https://github.com/Matt-Esch/anaconda");
-        ImGui::Text("http://mp2.dk/anaconda/");
-        ImGui::Text("http://mp2.dk/chowdren/");
+        ImGui::Text(R"(https://github.com/Matt-Esch/anaconda
+
+http://mp2.dk/anaconda/
+
+http://mp2.dk/chowdren/
+
+Anaconda is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Anaconda is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Anaconda.  If not, see <http://www.gnu.org/licenses/>.)");
         ImGui::TreePop();
     }
     ImGui::PopID();
