e33807584da7903f763c1fdec6b12f17c14e07a1
LAK132 <LAK132@kicodora.com>
2019-03-17 11:58:28 +1030

    Fixes #6

--- src/explorer.cpp
+++ src/explorer.cpp
@@ -1671,7 +1671,7 @@ namespace SourceExplorer
 
         switch (entry.mode)
         {
-            case MODE0: result = entry.readMode0(game, strm); break;
+            case MODE0: result = entry.readMode0(game, strm, 0x8); break;
             case MODE1: result = entry.readMode1(game, strm); break;
             case MODE2: result = entry.readMode2(game, strm); break;
             case MODE3: result = entry.readMode3(game, strm); break;
