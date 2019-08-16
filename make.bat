5faab667b8a3d1a4379b66bebe69035ddabde5ec
LAK132 <LAK132@kicodora.com>
2019-08-16 20:40:15 +0930

    Move compilers paths from makelist.bat to make.bat

--- make.bat
+++ make.bat
@@ -7,10 +7,30 @@ set target=%2
 if not "%mode%"=="debug" if not "%mode%"=="release" if not "%mode%"=="clean" goto useage
 if not "%target%"=="x86" if not "%target%"=="x64" if not "%mode%"=="clean" goto useage
 
-call makelist.bat %target% %mode%
+call makelist.bat
 
 if "%mode%"=="clean" goto clean
 
+if exist "vcvarsall.bat" (
+    call "vcvarsall.bat" %target%
+    goto begin
+)
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %target%
+    goto begin
+)
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vscarsall.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vscarsall.bat" %target%
+    goto begin
+)
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %target%
+    goto begin
+)
+set /p VCVARSALLBAT=vcvarsall.bat directory:
+call "%VCVARSALLBAT%" %target%
+
+:begin
 echo Compiling in %mode% mode for %target%
 
 title Compiler
--- makelist.bat
+++ makelist.bat
@@ -10,31 +10,9 @@ set BINARY=explorer.exe
 
 set CXX=cl /nologo /std:c++17 /D_CRT_SECURE_NO_WARNINGS /MD /EHsc
 
-if "%mode%"=="clean" goto :eof
-
-if exist "vcvarsall.bat" (
-    call "vcvarsall.bat" %1
-    goto mode
-)
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %1
-    goto mode
-)
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vscarsall.bat" (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vscarsall.bat" %1
-    goto mode
-)
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %1
-    goto mode
-)
-set /p VCVARSALLBAT=vcvarsall.bat directory:
-call "%VCVARSALLBAT%" %1
-
-
-:mode
 if "%mode%"=="release" goto release
 if "%mode%"=="debug" goto debug
+goto :eof
 
 :debug
 set COMPFLAGS=/Zi /bigobj /O2
