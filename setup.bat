559e2a42f890354dafe1f33df4524e6c840d3aab
LAK132 <lakridesagain@gmail.com>
2021-10-24 01:44:35 +1030

    fix packing problem that was causing a crash

--- setup.bat
+++ setup.bat
@@ -5,5 +5,5 @@ call msvc.bat %~1
 
 rmdir /s /q build
 @REM meson setup build --buildtype release || exit 1
-@REM meson setup build --buildtype debug || exit 1
-meson setup build --buildtype debugoptimized || exit 1
+meson setup build --buildtype debug || exit 1
+@REM meson setup build --buildtype debugoptimized || exit 1
