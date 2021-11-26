cd3b6c3a8a349836e9e23b3541bd171994af7dc3
LAK132 <lakridesagain@gmail.com>
2021-11-26 14:33:19 +1030

    add generate_git_file.sh

---
+++ generate_git_file.sh
@@ -0,0 +1,8 @@
+git_hash="`git rev-parse --short HEAD`" || exit 1
+git_tag="`git describe --tags --abbrev=0`" || exit 1
+echo "#ifndef GIT_HASH" > $1
+echo "#define GIT_HASH "\"$git_hash\" >> $1
+echo "#endif" >> $1
+echo "#ifndef GIT_TAG" >> $1
+echo "#define GIT_TAG "\"$git_tag\" >> $1
+echo "#endif" >> $1
