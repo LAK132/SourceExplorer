288339228d347323caa8fbaaa9c04ea2822448ca
LAK132 <lakridesagain@gmail.com>
2021-11-26 15:45:09 +1030

    use --always fallback

--- generate_git_file.bat
+++ generate_git_file.bat
@@ -1,6 +1,6 @@
 @echo off
 for /f %%f in ('git rev-parse --short HEAD') do (set "git_hash=%%f")
-for /f %%f in ('git describe --tags --abbrev^=0') do (set "git_tag=%%f")
+for /f %%f in ('git describe --tags --always --abbrev^=0') do (set "git_tag=%%f")
 (
 echo #ifndef GIT_HASH
 echo #define GIT_HASH "%git_hash%"
--- generate_git_file.sh
+++ generate_git_file.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 git_hash="`git rev-parse --short HEAD`" || exit 1
-git_tag="`git describe --tags --abbrev=0`" || exit 1
+git_tag="`git describe --tags --always --abbrev=0`" || exit 1
 echo "#ifndef GIT_HASH" > $1
 echo "#define GIT_HASH "\"$git_hash\" >> $1
 echo "#endif" >> $1
